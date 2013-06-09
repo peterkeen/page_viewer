f0d16f7c4e958a3d259d460443f3f6247674cd90
Pete Keen <pete@bugsplat.info>
2013-06-09 13:41:52 -0700

    Add PDF routes

--- Gemfile
+++ Gemfile
@@ -4,4 +4,4 @@ gem 'sinatra'
 gem 'redcarpet'
 gem 'pygments.rb'
 gem 'capistrano-buildpack'
-
+gem 'docverter'
--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
 require 'redcarpet'
 require 'pygments'
+require 'docverter'
+
+Docverter.base_url = 'http://c.docverter.com'
 
 PAGE_ROOT = ARGV[-1]
 unless PAGE_ROOT
@@ -11,6 +14,10 @@ end
 class HTMLwithPygments < Redcarpet::Render::HTML
   def block_code(code, language)
     Pygments.highlight(code, :lexer => language)
+  end
+
+  def postprocess(document)
+    document.gsub('&#39;', "'")
   end
 end
 
@@ -32,8 +39,41 @@ get '/:page.md' do
   File.read(File.join(PAGE_ROOT, params[:page] + ".md"))
 end
 
+
+get '/_book.pdf' do
+  chapters = File.read(File.join(PAGE_ROOT, 'chapters')).split("\n")
+  raw = ""
+  chapters.each do |chapter|
+    raw << File.read(File.join(PAGE_ROOT, chapter + ".md"))
+  end
+
+  @content = RENDERER.render(raw)
+
+  pdf = erb :page, :layout => :pdf
+  content_type 'application/pdf'
+  Docverter::Conversion.run do |c|
+    c.from = 'html'
+    c.to = 'pdf'
+    c.content = pdf
+  end
+end
+
+get '/:page.pdf' do
+  @content = RENDERER.render(File.read(File.join(PAGE_ROOT, params[:page] + ".md")))
+  @title = params[:page].gsub('_', ' ')
+  pdf = erb :page, :layout => :pdf
+  p pdf
+  content_type 'application/pdf'
+  Docverter::Conversion.run do |c|
+    c.from = 'html'
+    c.to = 'pdf'
+    c.content = pdf
+  end
+end
+
 get '/:page' do
   @content = RENDERER.render(File.read(File.join(PAGE_ROOT, params[:page] + ".md")))
   @title = params[:page].gsub("_", " ")
   erb :page
 end
+
